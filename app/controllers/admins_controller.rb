e62a906f006f6b83f6f16ee5d82a860d5e52e4a5
ehachi <ehajianto@gmail.com>
2023-04-01 12:14:35 +0300

    added controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,2 +1,35 @@
 class AdminsController < ApplicationController
-end
+
+    skip_before_action :authorized, only: [:create]
+  
+    def profile
+      render json: { admin: AdminSerializer.new(current_admin) }, status: :accepted
+    end
+  
+    def create 
+      admin = Admin.create(admin_params)
+      if admin.valid?
+        token = encode_token({ admin_id: admin.id })
+        render json: { success: 'admin saved successfully!', jwt: token }
+      else
+        render json: admin.errors.messages, status: :unprocessable_entity
+      end
+    end
+  
+    def show
+      admin = current_admin.admins.find_by(id: params[:id])
+      if admin
+        render json: admin
+      else
+        render json: { error: "Admin not found" }, status: :not_found
+      end
+    end
+    
+      
+    private
+    
+    def admin_params
+      params.permit(:adminname, :email, :password)
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :admins
+  resources :users, only: [:show]
+  resources :gyms
+  resources :admins, only: [:create, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
