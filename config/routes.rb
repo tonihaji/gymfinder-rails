fc9035b5a934013ede89f8395f63e857385899d9
ehachi <ehajianto@gmail.com>
2023-04-01 12:32:45 +0300

    modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show]
-  resources :gyms
-  resources :admins, only: [:create, :show]
+  # resources :users, only: [:show]
+  # resources :gyms
+  # resources :admins, only: [:create, :show]
+  get "/", to: "admins#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
