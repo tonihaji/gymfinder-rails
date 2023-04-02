5e15ac766b30072f763b016fbe27ef98754594e2
ehachi <ehajianto@gmail.com>
2023-04-02 21:46:14 +0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-puts "done seeding..."
-puts "creating admins..."
-Admin.create(adminname: 'admin1', email: 'admin1@example.com', password: 'password')
-Admin.create(adminname: 'admin2', email: 'admin2@example.com', password: 'password')
+# puts "done seeding..."
+# puts "creating admins..."
+# Admin.create(adminname: 'admin1', email: 'admin1@example.com', password: 'password')
+# Admin.create(adminname: 'admin2', email: 'admin2@example.com', password: 'password')
 
-puts "creating gym record of admin1..."
-Gym.create(image: 'https://geometriktarget.com/wp-content/uploads/2022/06/abrir_ginasio_BLOG.jpg', name: 'Gym A', location: '123 Main St', operatingHours: '9am-10pm', price: 50, contact: +254712345678, admin_id: 1)
-Gym.create(image: 'https://www.stanleyhomesinc.com/wp-content/uploads/2015/04/LuxuryHomeFitnessGym.jpg', name: 'Gym B', location: '456 Oak St', operatingHours: '7am-9pm', price: 60, contact: +254712345678, admin_id: 1)
-Gym.create(image: 'https://geometriktarget.com/wp-content/uploads/2022/06/abrir_ginasio_BLOG.jpg', name: 'Gym C', location: '789 Pine St', operatingHours: '10am-8pm', price: 70, contact: +254712345678, admin_id: 1)
+# puts "creating gym record of admin1..."
+# Gym.create(image: 'https://geometriktarget.com/wp-content/uploads/2022/06/abrir_ginasio_BLOG.jpg', name: 'Gym A', location: '123 Main St', operatingHours: '9am-10pm', price: 50, contact: +254712345678, admin_id: 1)
+# Gym.create(image: 'https://www.stanleyhomesinc.com/wp-content/uploads/2015/04/LuxuryHomeFitnessGym.jpg', name: 'Gym B', location: '456 Oak St', operatingHours: '7am-9pm', price: 60, contact: +254712345678, admin_id: 1)
+# Gym.create(image: 'https://geometriktarget.com/wp-content/uploads/2022/06/abrir_ginasio_BLOG.jpg', name: 'Gym C', location: '789 Pine St', operatingHours: '10am-8pm', price: 70, contact: +254712345678, admin_id: 1)
 
-puts  "creating gym records of admin2..."
-Gym.create(image: 'https://www.shutterstock.com/image-photo/modern-light-gym-sports-equipment-260nw-721723381.jpg', name: 'Gym D', location: '321 Elm St', operatingHours: '8am-11pm', price: 55, contact: +254712345678, admin_id: 2)
-Gym.create(image: 'https://www.leisureopportunities.co.uk/images/2022/862138_89821.jpg', name: 'Gym E', location: '654 Cedar St', operatingHours: '6am-10pm', price: 65, contact: +254712345678, admin_id: 2)
-puts "done seeding..."
+# puts  "creating gym records of admin2..."
+# Gym.create(image: 'https://www.shutterstock.com/image-photo/modern-light-gym-sports-equipment-260nw-721723381.jpg', name: 'Gym D', location: '321 Elm St', operatingHours: '8am-11pm', price: 55, contact: +254712345678, admin_id: 2)
+# Gym.create(image: 'https://www.leisureopportunities.co.uk/images/2022/862138_89821.jpg', name: 'Gym E', location: '654 Cedar St', operatingHours: '6am-10pm', price: 65, contact: +254712345678, admin_id: 2)
+# puts "done seeding..."
