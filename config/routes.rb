9ab17982deb628e9cf52014a32709b0712060904
ehachi <ehajianto@gmail.com>
2023-04-02 13:35:09 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,30 @@ Rails.application.routes.draw do
   # resources :users
   # resources :gyms
 
-  resources :users, only: [:show]
-  resources :gyms
-  resources :admins, only: [:create]
+  # resources :users, only: [:show]
+  # resources :gyms
+  # resources :admins, only: [:create]
   get '/current_admin', to: 'application#current_admin'
   get '/profile', to: 'admins#profile'
   post '/login', to: 'auth#create'
+
+#gym routes
+get "/", to: "gyms#index"
+get '/gyms/:id', to: 'gyms#show', 
+post '/gyms', to: 'gyms#create', 
+delete '/gyms/:id', to: 'gyms#destroy'
+
+# users
+get "users", to: "users#index"
+get '/users/:id', to: 'users#show', 
+post '/users', to: 'users#create', 
+delete '/users/:id', to: 'users#destroy'
+
+# admins
+get "admins", to: "admins#index"
+get '/admins/:id', to: 'admins#show', 
+post '/admins', to: 'admins#create', 
+delete '/admins/:id', to: 'admins#destroy'
+
+
 end
