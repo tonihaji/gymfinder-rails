3c1c23ee16eba96c3095d8422c04c804b9ad159e
ehachi <ehajianto@gmail.com>
2023-04-02 13:48:51 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,22 +12,22 @@ Rails.application.routes.draw do
   post '/login', to: 'auth#create'
 
 #gym routes
-get "/", to: "gyms#index"
-get '/gyms/:id', to: 'gyms#show', 
-post '/gyms', to: 'gyms#create', 
-delete '/gyms/:id', to: 'gyms#destroy'
+get "/", to: "gym#index"
+# get '/gyms/:id', to: 'gyms#show', 
+# post '/gyms', to: 'gyms#create', 
+# delete '/gyms/:id', to: 'gyms#destroy'
 
-# users
-get "users", to: "users#index"
-get '/users/:id', to: 'users#show', 
-post '/users', to: 'users#create', 
-delete '/users/:id', to: 'users#destroy'
+# # users
+# get "users", to: "users#index"
+# get '/users/:id', to: 'users#show', 
+# post '/users', to: 'users#create', 
+# delete '/users/:id', to: 'users#destroy'
 
-# admins
-get "admins", to: "admins#index"
-get '/admins/:id', to: 'admins#show', 
-post '/admins', to: 'admins#create', 
-delete '/admins/:id', to: 'admins#destroy'
+# # admins
+# get "admins", to: "admins#index"
+# get '/admins/:id', to: 'admins#show', 
+# post '/admins', to: 'admins#create', 
+# delete '/admins/:id', to: 'admins#destroy'
 
 
 end
