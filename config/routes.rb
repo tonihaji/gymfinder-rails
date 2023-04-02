1c303bfb48090e52e82a99c061eb574110163141
ehachi <ehajianto@gmail.com>
2023-04-02 13:59:00 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,9 @@ Rails.application.routes.draw do
 
 #gym routes
 get "/", to: "gyms#index"
-# get '/gyms/:id', to: 'gyms#show', 
-# post '/gyms', to: 'gyms#create', 
-# delete '/gyms/:id', to: 'gyms#destroy'
+get '/gyms/:id', to: 'gyms#show', 
+post '/gyms', to: 'gyms#create', 
+delete '/gyms/:id', to: 'gyms#destroy'
 
 # # users
 # get "users", to: "users#index"
